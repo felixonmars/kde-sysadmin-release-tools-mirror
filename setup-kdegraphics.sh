c7b8626703acea8e3560c20c83b48899f82f8832
Dirk Mueller <mueller@kde.org>
2011-04-01 19:39:41 +0000

    add a script to reassemble kdegraphics
    svn path=/trunk/kde-common/release/; revision=1226732

---
+++ setup-kdegraphics.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# vim: sw=4 et
+
+set -x
+set -e
+
+setup_branch_checkout()
+{
+    git branch --track KDE/4.6 remotes/origin/KDE/4.6
+    git checkout KDE/4.6
+}
+
+svn export $BASE/branches/KDE/4.6/kdegraphics
+(
+    cd kdegraphics
+
+    for d in kamera kgamma kcolorchooser gwenview kolourpaint ksaneplugin ksnapshot kruler svgpart; do
+        rm -rf $d
+        rm -rf doc/$d
+        sed -i -e "s,add_subdirectory.*$d.*,," doc/CMakeLists.txt
+
+        git clone git@git.kde.org:$d
+        ( cd $d; setup_branch_checkout )
+    done
+
+    (
+        cd libs
+        for d in libkdcraw libkexiv2 libkipi libksane; do
+            rm -rf $d
+            git clone git@git.kde.org:$d
+            (cd $d; setup_branch_checkout )
+        done
+    )
+
+    rm -rf strigi-analyzer
+    git clone git@git.kde.org:kdegraphics-strigi-analyzer strigi-analyzer
+    ( cd strigi-analyzer; setup_branch_checkout )
+
+    rm -rf thumbnailers
+    git clone git@git.kde.org:kdegraphics-thumbnailers thumbnailers
+    ( cd thumbnailers; setup_branch_checkout )
+
+)
