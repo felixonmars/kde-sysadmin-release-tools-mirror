8959a57b0540b5689ccd0841c5274e919904639f
Tom Albers <toma@kde.org>
2011-04-23 13:16:24 +0000

    One more header.
    svn path=/trunk/playground/utils/releaseschedule/; revision=1228912

--- tools/releaseschedule/mainclass.cpp
+++ tools/releaseschedule/mainclass.cpp
@@ -313,6 +313,7 @@ void MainClass::slotGenerateICal()
         text.append( "DTSTART;VALUE=DATE:" + dt.toString( "yyyyMMdd" ) + "\r\n" );
         text.append( "DTEND;VALUE=DATE:" + dt.addDays(1).toString( "yyyyMMdd" ) + "\r\n" );
         text.append( "X-MICROSOFT-CDO-ALLDAYEVENT:TRUE\r\n" );
+        text.append( "X-MICROSOFT-CDO-BUSYSTATUS:FREE\r\n" );
         text.append( "X-MICROSOFT-CDO-INTENDEDSTATUS:FREE\r\n" );
         text.append( "SUMMARY:" + i.value().first + "\r\n" );
         QString desc(i.value().second);
