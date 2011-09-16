f7adbabf4fb5d51508585ecd47a24804abf45f45
Jeremy Paul Whiting <jpwhiting@kde.org>
2011-09-16 03:44:47 +0000

    kdeedu branches conform to KDE/X.Y now, as does kdeplasma-addons
    svn path=/trunk/kde-common/release/; revision=1253889

--- setup-kdeedu.sh
+++ setup-kdeedu.sh
@@ -6,22 +6,16 @@ set -e
 
 setup_branch_checkout()
 {
-    git branch --track 4.6 remotes/origin/4.6
-    git checkout 4.6
+    git checkout KDE/4.7
 }
 
-svn export -N $BASE/tags/KDE/4.6.2/kdeedu
+svn export -N $BASE/tags/KDE/4.7.0/kdeedu
 (
     cd kdeedu
 
-    for d in blinken cantor kalgebra kalzium kanagram kbruch kgeography khangman kig kiten klettres kmplot kstars ktouch kturtle kwordquiz libkdeedu parley rocs step; do
+    for d in blinken cantor kalgebra kalzium kanagram kbruch kgeography khangman kig kiten klettres kmplot kstars ktouch kturtle kwordquiz libkdeedu marble parley rocs step; do
 
         git clone git@git.kde.org:$d
         ( cd $d; setup_branch_checkout )
     done
-
-
-    git clone git@git.kde.org:marble
-    (cd marble; git checkout kde-4.6 )
-
 )
