fc03906ab20aac62e569dd676b0e49b29215a187
Torgny Nyblom <nyblom@kde.org>
2013-02-19 20:50:58 +0100

    Add script for getting all used revisions and hashes

---
+++ get_revisions_and_hashes.sh
@@ -0,0 +1,11 @@
+rm REVISIONS_AND_HASHES
+
+for d in clean/*; do
+    pushd ${d}
+    if [[ -d .git ]]; then
+        echo ${d#clean/} `cat .git/refs/heads/KDE/4.9` >> ../../REVISIONS_AND_HASHES
+    elif [[ -d .svn ]]; then
+        echo ${d#clean/} `svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p'` >> ../../REV    ISIONS_AND_HASHES
+    fi
+    popd
+done
