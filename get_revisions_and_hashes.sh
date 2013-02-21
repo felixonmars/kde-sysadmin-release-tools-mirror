ae4fbd6b9aa41c1847cd6308ae75bd535a6e2dfd
Albert Astals Cid <aacid@kde.org>
2013-02-21 17:17:21 +0100

    Fix typo
    Torgny do we want in the 4.10 branch too?
    CCMAIL: nyblom@kde.org

--- get_revisions_and_hashes.sh
+++ get_revisions_and_hashes.sh
@@ -5,7 +5,7 @@ for d in clean/*; do
     if [[ -d .git ]]; then
         echo ${d#clean/} `cat .git/refs/heads/KDE/4.9` >> ../../REVISIONS_AND_HASHES
     elif [[ -d .svn ]]; then
-        echo ${d#clean/} `svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p'` >> ../../REV    ISIONS_AND_HASHES
+        echo ${d#clean/} `svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p'` >> ../../REVISIONS_AND_HASHES
     fi
     popd
 done
