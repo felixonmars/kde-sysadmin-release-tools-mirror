491e6db707a42897e93673431fea811bcc0f050a
Torgny Nyblom <nyblom@kde.org>
2013-02-24 07:02:01 +0100

    Use master as ref

--- get_revisions_and_hashes.sh
+++ get_revisions_and_hashes.sh
@@ -3,9 +3,9 @@ rm REVISIONS_AND_HASHES
 for d in clean/*; do
     pushd ${d}
     if [[ -d .git ]]; then
-        echo ${d#clean/} `cat .git/refs/heads/KDE/4.9` >> ../../REVISIONS_AND_HASHES
+        echo ${d#clean/} `cat .git/refs/heads/master` | tee -a ../../REVISIONS_AND_HASHES
     elif [[ -d .svn ]]; then
-        echo ${d#clean/} `svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p'` >> ../../REVISIONS_AND_HASHES
+        echo ${d#clean/} `svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p'` | tee -a ../../REVISIONS_AND_HASHES
     fi
     popd
 done
