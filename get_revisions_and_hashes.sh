607718afd511877d4dffecd3b1d202b9e01d4d92
Torgny Nyblom <nyblom@kde.org>
2013-09-04 21:04:45 +0200

    Don't print dir when getting revisions and hashes

--- get_revisions_and_hashes.sh
+++ get_revisions_and_hashes.sh
@@ -1,11 +1,11 @@
 rm REVISIONS_AND_HASHES
 
 for d in clean/*; do
-    pushd ${d}
+    pushd ${d} 2>&1 > /dev/null
     if [[ -d .git ]]; then
         echo ${d#clean/} `cat .git/refs/heads/master` | tee -a ../../REVISIONS_AND_HASHES
     elif [[ -d .svn ]]; then
         echo ${d#clean/} `svn info | sed -n -e '/^Revision: \([0-9]*\).*$/s//\1/p'` | tee -a ../../REVISIONS_AND_HASHES
     fi
-    popd
+    popd 2>&1 > /dev/null
 done
