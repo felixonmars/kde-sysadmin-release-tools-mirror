f36e8a746c4c36f4ebf7322f262c1c3f2644c339
Albert Astals Cid <aacid@kde.org>
2014-03-13 23:19:37 +0100

    tag_all now also tags svn

--- tag_all.sh
+++ tag_all.sh
@@ -24,8 +24,22 @@ cat $here/modules.git | while read repo branch; do
         exit 3
     fi
     echo $PWD
-    git fetch
+    git fetch || exit 2
     git tag -a $tagname $b -m "Create tag for $version"  || exit 4
     git push --tags || exit 5
 done
 
+
+. $here/version
+svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version -m 'Create tag for $version' || exit 7
+cat $here/modules.svn | while read repo branch; do
+    echo $repo
+    . $here/version
+    tagname=v$version
+    versionfile=$here/versions/$repo
+    if [ ! -f $versionfile ]; then echo "$versionfile not found"; exit 6; fi
+    b=`sed '2q;d' $versionfile`
+    echo $b
+    branch=`echo $branch | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
+    svn cp $branch/$repo@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/ -m 'Create tag for $version' || exit 8
+done
