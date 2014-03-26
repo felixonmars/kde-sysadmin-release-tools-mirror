9b4fbab0004403c049e6e4c244becd938e748037
Albert Astals Cid <aacid@kde.org>
2014-03-26 20:27:21 +0100

    Bring svn tagging from the 4.13 branch

--- tag_all.sh
+++ tag_all.sh
@@ -8,6 +8,11 @@ here=$PWD
 # TODO: update this path
 srcdir=/d/kde/src
 
+if [ ! -d $srcdir ]; then
+    echo "$srcdir does not exist, please fix srcdir variable"
+    exit
+fi
+
 cat $here/modules.git | while read repo branch; do
     cd $srcdir || exit 1
     echo $repo
@@ -24,8 +29,41 @@ cat $here/modules.git | while read repo branch; do
         exit 3
     fi
     echo $PWD
-    git fetch
+    git fetch || exit 2
     git tag -a $tagname $b -m "Create tag for $version"  || exit 4
     git push --tags || exit 5
 done
 
+
+. $here/version
+svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version -m "Create tag for $version" || exit 7
+cat $here/modules.svn | while read repo branch; do
+    echo $repo
+    . $here/version
+    tagname=v$version
+    versionfile=$here/versions/$repo
+    if [ ! -f $versionfile ]; then echo "$versionfile not found"; exit 6; fi
+    b=`sed '2q;d' $versionfile`
+    echo $b
+    branch=`echo $branch | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
+    svn cp $branch/$repo@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/ -m "Create tag for $version" || exit 8
+done
+
+svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 9
+for lang in `cat language_list`; do
+    echo $lang
+    . l10n_branch
+    . $here/version
+    versionfile=$here/versions/kde-l10n-$lang
+    if [ ! -f $versionfile ]; then echo "$versionfile not found"; exit 10; fi
+    b=`sed '2q;d' $versionfile`
+    echo $b
+    branch=`echo $branch | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
+    echo $branch
+    svn cp $branch/$lang@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 11
+    variants=`svn cat svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n/$lang/pack-with-variants 2> /dev/null` || continue
+    for variant in $variants; do
+        echo $variant
+        svn cp $branch/$variant@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 12
+    done
+done
