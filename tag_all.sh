33f40ac92514351986d76aae8bdce798612cc5c9
l10n daemon script <scripty@kde.org>
2014-08-01 07:57:47 +0000

    More fixes for the 5.0 release

--- tag_all.sh
+++ tag_all.sh
@@ -7,6 +7,11 @@ here=$PWD
 . config
 . utils.sh
 
+cmd=
+if [ "$dry_run" = "1" ]; then
+    cmd=echo
+fi
+
 if [ ! -d $srcdir ]; then
     echo "$srcdir does not exist, please fix srcdir variable"
     exit
@@ -23,17 +28,15 @@ cat $here/modules.git | while read repo branch; do
     checkout=$(findCheckout $repo)
     cd $checkout || exit 2
     echo $PWD
-    git fetch || exit 2
-    git tag -a $tagname $b -m "Create tag for $version"  || exit 4
-    git push --tags || exit 5
+    $cmd git fetch || exit 2
+    $cmd git tag -a $tagname $b -m "Create tag for $version"  || exit 4
+    $cmd git push --tags || exit 5
 done
-
-. $here/config
 
 if [ "$release_l10n_separately" = 1 ]; then
 
     . $here/version
-    svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version -m "Create tag for $version" || exit 7
+    $cmd svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version -m "Create tag for $version" || exit 7
     cat $here/modules.svn | while read repo branch; do
         echo $repo
         . $here/version
@@ -43,11 +46,11 @@ if [ "$release_l10n_separately" = 1 ]; then
         b=`sed '2q;d' $versionfile`
         echo $b
         branch=`echo $branch | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
-        svn cp $branch/$repo@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/ -m "Create tag for $version" || exit 8
+        $cmd svn cp $branch/$repo@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/ -m "Create tag for $version" || exit 8
     done
 
     l10n_repo=`echo $l10n_repo | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
-    svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 9
+    $cmd svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 9
     for lang in `cat language_list`; do
         echo $lang
         . $here/version
@@ -56,11 +59,11 @@ if [ "$release_l10n_separately" = 1 ]; then
         b=`sed '2q;d' $versionfile`
         echo $b
         echo $l10n_repo
-        svn cp $l10n_repo/$lang@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 11
+        $cmd svn cp $l10n_repo/$lang@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 11
         variants=`svn cat svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n/$lang/pack-with-variants 2> /dev/null` || continue
         for variant in $variants; do
             echo $variant
-            svn cp $l10n_repo/$variant@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 12
+            $cmd svn cp $l10n_repo/$variant@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 12
         done
     done
 fi
