335d4da911d16eff465de05282811b7087f56251
David Faure <faure@kde.org>
2014-08-10 22:35:42 +0200

    small fixes

--- upload_all.sh
+++ upload_all.sh
@@ -7,7 +7,7 @@
 
 dest=stable/frameworks/$version
 
-ssh ftpadmin@depot.kde.org "mkdir $dest"
+ssh ftpadmin@depot.kde.org "mkdir -p $dest"
 ssh ftpadmin@depot.kde.org "chmod o-rx $dest"
 rsync --progress -v -a -e "ssh -x" -r sources/* ftpadmin@depot.kde.org:$dest/
 
