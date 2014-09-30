fe96febe7e5ccf20a067e94ff8c24014d217679a
David Faure <faure@kde.org>
2014-09-30 09:53:18 +0200

    Detect new languages

--- update_l10n.sh
+++ update_l10n.sh
@@ -6,33 +6,19 @@
 . config
 unset CDPATH
 
-if test -d l10n; then
-
-    # Update existing checkout
-
-    for dir in l10n/*/messages/$l10n_module; do
-        ( cd $dir ; svn cleanup ; svn update )
-    done
-    for dir in l10n/*/docs/$l10n_module; do
-        ( cd $dir ; svn cleanup ; svn update )
-    done
-    for dir in l10n/*/scripts/$l10n_module; do
-        ( cd $dir ; svn cleanup ; svn update )
-    done
-
-else
-
-    # Make new checkout
-
-    languages=`svn cat $l10n_repo/subdirs`
-    for lang in $languages; do
+languages=`svn cat $l10n_repo/subdirs`
+for lang in $languages; do
+    if test -d l10n/$lang/messages/$l10n_module; then
+        ( cd l10n/$lang/messages/$l10n_module ; svn cleanup ; svn update )
+        test -d l10n/$lang/docs/$l10n_module && ( cd l10n/$lang/docs/$l10n_module ; svn cleanup ; svn update )
+        test -d l10n/$lang/scripts/$l10n_module && ( cd l10n/$lang/scripts/$l10n_module ; svn cleanup ; svn update )
+    else
+        # Make new checkout
         mkdir -p l10n/$lang/messages
         svn co $l10n_repo/$lang/messages/$l10n_module l10n/$lang/messages/$l10n_module
         mkdir -p l10n/$lang/docs
         svn co $l10n_repo/$lang/docs/$l10n_module l10n/$lang/docs/$l10n_module
         mkdir -p l10n/$lang/scripts
         svn co $l10n_repo/$lang/scripts/$l10n_module l10n/$lang/scripts/$l10n_module
-    done
-
-fi
-
+    fi
+done
