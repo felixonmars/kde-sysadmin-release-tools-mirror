dff09a6f75ba1c53e4e6d90777217bd470afa7ab
David Faure <faure@kde.org>
2014-10-04 19:33:24 +0200

    streamlining the process

--- grab_from_scripty.sh
+++ grab_from_scripty.sh
@@ -2,7 +2,6 @@
 
 rm -f REVISIONS_AND_HASHES tags.git to_commit.diff
 scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/REVISIONS_AND_HASHES .
-scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/tags.git .
 scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/to_commit.diff .
 scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/changelog .
 git apply to_commit.diff
