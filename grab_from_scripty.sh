b52aa242aaded911303907aa16178c7f475b056b
David Faure <faure@kde.org>
2014-10-06 23:08:39 +0200

    improvements to the scripts, for KF 5.3

--- grab_from_scripty.sh
+++ grab_from_scripty.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 rm -f REVISIONS_AND_HASHES tags.git to_commit.diff
-scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/REVISIONS_AND_HASHES .
-scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/to_commit.diff .
-scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/changelog .
-git apply to_commit.diff
+ssh scripty@l10n.kde.org 'cd frameworks_packaging/release-tools ; git diff > to_commit.diff'
+scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/{REVISIONS_AND_HASHES,to_commit.diff,changelog} .
+test -s to_commit.diff && git apply to_commit.diff
