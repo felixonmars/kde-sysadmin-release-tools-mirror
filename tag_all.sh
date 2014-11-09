4b68f628622d40eac31e200c97685e8d175ebc2a
Albert Astals Cid <aacid@kde.org>
2014-11-09 23:35:19 +0100

    svn modules don't depend on if l10n is packaged separately or not
    Also KDE/$version -> Applications/$version

--- tag_all.sh
+++ tag_all.sh
@@ -38,26 +38,24 @@ cat $here/modules.git | while read repo branch; do
     git push --tags || exit 5
 done
 
+. $here/version
+svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/Applications/$version -m "Create tag for $version" || exit 7
+cat $here/modules.svn | while read repo branch; do
+	echo $repo
+	. $here/version
+	tagname=v$version
+	versionfile=$here/versions/$repo
+	if [ ! -f $versionfile ]; then echo "$versionfile not found"; exit 6; fi
+	b=`sed '2q;d' $versionfile`
+	echo $b
+	branch=`echo $branch | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
+	svn cp $branch/$repo@$b svn+ssh://svn@svn.kde.org/home/kde/tags/Applications/$version/ -m "Create tag for $version" || exit 8
+done
+
 . $here/config
-
 if [ "$release_l10n_separately" = 1 ]; then
-
-    . $here/version
-    svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version -m "Create tag for $version" || exit 7
-    cat $here/modules.svn | while read repo branch; do
-        echo $repo
-        . $here/version
-        tagname=v$version
-        versionfile=$here/versions/$repo
-        if [ ! -f $versionfile ]; then echo "$versionfile not found"; exit 6; fi
-        b=`sed '2q;d' $versionfile`
-        echo $b
-        branch=`echo $branch | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
-        svn cp $branch/$repo@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/ -m "Create tag for $version" || exit 8
-    done
-
     l10n_repo=`echo $l10n_repo | sed 's#svn://anonsvn.kde.org#svn+ssh://svn@svn.kde.org#g'`
-    svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 9
+    svn mkdir svn+ssh://svn@svn.kde.org/home/kde/tags/Applications/$version/kde-l10n -m "Create tag for $version" || exit 9
     for lang in `cat language_list`; do
         echo $lang
         . $here/version
@@ -66,11 +64,11 @@ if [ "$release_l10n_separately" = 1 ]; then
         b=`sed '2q;d' $versionfile`
         echo $b
         echo $l10n_repo
-        svn cp $l10n_repo/$lang@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 11
-        variants=`svn cat svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n/$lang/pack-with-variants 2> /dev/null` || continue
+        svn cp $l10n_repo/$lang@$b svn+ssh://svn@svn.kde.org/home/kde/tags/Applications/$version/kde-l10n -m "Create tag for $version" || exit 11
+        variants=`svn cat svn+ssh://svn@svn.kde.org/home/kde/tags/Applications/$version/kde-l10n/$lang/pack-with-variants 2> /dev/null` || continue
         for variant in $variants; do
             echo $variant
-            svn cp $l10n_repo/$variant@$b svn+ssh://svn@svn.kde.org/home/kde/tags/KDE/$version/kde-l10n -m "Create tag for $version" || exit 12
+            svn cp $l10n_repo/$variant@$b svn+ssh://svn@svn.kde.org/home/kde/tags/Applications/$version/kde-l10n -m "Create tag for $version" || exit 12
         done
     done
 fi
