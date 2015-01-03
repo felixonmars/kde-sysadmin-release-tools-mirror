b6d17e167bad7808b1329c771369c09747ab451e
David Faure <faure@kde.org>
2015-01-03 14:08:53 +0100

    5.6 release

--- grab_from_scripty.sh
+++ grab_from_scripty.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 rm -f REVISIONS_AND_HASHES tags.git to_commit.diff
+mv -f changelog changelog.orig
 ssh scripty@l10n.kde.org 'cd frameworks_packaging/release-tools ; git diff > to_commit.diff'
-scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/{REVISIONS_AND_HASHES,to_commit.diff} .
+scp scripty@l10n.kde.org:~/frameworks_packaging/release-tools/{REVISIONS_AND_HASHES,to_commit.diff,changelog} .
 test -s to_commit.diff && git apply to_commit.diff
